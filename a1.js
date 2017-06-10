f747e5b26666e0541888e8f5008deec3e5c83c2b
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-06-10 19:18:08 +0200

    b6

--- a1.js
+++ a1.js
@@ -1,9 +1,9 @@
 'use strict'
 
-let greet = function(parameter1, parameter2) {
-    console.log(parameter1 + " " + parameter2);
+let greet = function(parameter1) {
+    console.log(parameter1);
 }
 
-let parameter1 = "JavaScript";
-let parameter2 = "is kinda cool";
-greet(parameter1, parameter2);
+let parameter1 = "JavaScript is very cool";
+
+greet(parameter1);
